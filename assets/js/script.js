66dae90a91c5efb0bd3d970fff47abf2e836e8e3
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-08-03 00:38:47 -0400

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,14 @@ let apiURL =
   "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=d33c2ae08fb898222095f60271a74087";
 
 const searchHistoryEl = document.getElementById("searchHistory");
+const searchInputEl = document.getElementById("searchInput");
+const searchButtonEl = document.getElementById("searchButton");
 
 function displaySavedLocations() {
   let locations = localStorage.getItem("recentSearches");
-  if (locations) {
+  if (locations == null) {
+    return;
+  } else {
     let parsedLocations = JSON.parse(locations);
     parsedLocations.forEach(function (item) {
       let listItem = document.createElement("li");
@@ -16,7 +20,24 @@ function displaySavedLocations() {
   }
 }
 
+function updateContentPane(event) {
+  event.preventDefault();
+  const buttonClicked = event.target;
+  let location = buttonClicked.getAttribute("data-location");
+}
+function getLocation(event) {
+  event.preventDefault();
+  let location = searchInputEl.nodeValue;
+  window.alert(location);
+}
+
+function setEventListeners() {
+  searchHistoryEl.addEventListener("click", updateContentPane());
+  searchButtonEl.addEventListener("click", getLocation());
+}
+
 function init() {
+  setEventListeners();
   displaySavedLocations();
 }
 
