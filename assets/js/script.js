049cb2c8e5c77f3242a63110a29723cf6224872c
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-08-06 23:12:47 -0400

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,42 @@ const searchHistoryEl = document.getElementById("searchHistory");
 const searchInputEl = document.getElementById("searchInput");
 const searchButtonEl = document.getElementById("searchButton");
 const clearButtonEl = document.getElementById("clearButton");
+const locationNameEl = document.getElementById("locationName");
+const currentDayEl = document.getElementById("currentDay");
+const currentTempEl = document.getElementById("currentTemp");
+const currentWindEl = document.getElementById("currentWindSpeed");
+const currentHumidityEl = document.getElementById("currentHumidity");
+const uvIndexEl = document.getElementById("uvIndex");
 const locationHistory = JSON.parse(localStorage.getItem("recentSearches"));
-let locationSearch = [];
-let recentSearches = [];
+const storedLocationButtons = JSON.parse(
+  localStorage.getItem("locationButtons")
+);
 
 function displaySavedLocations() {
-  locationHistory.forEach((element) => {
-    createLocationButton(element);
-  });
+  if (locationHistory !== null) {
+    locationHistory.forEach((element) => {
+      console.log(storedLocationButtons);
+    });
+  }
 }
 
 function createLocationButton(location) {
-  let duplicateLocation = function checkForDuplicate(recentSearches, location) {
-    return recentSearches.some(function (loc) {
-      return location.toLowerCase() === loc.toLowerCase();
-    });
-  };
-  if (!duplicateLocation(recentSearches, location)) {
-    let listItem = document.createElement("li");
-    let content = `<button data-location="${location}">${
-      location[0].toUpperCase() + location.substring(1)
-    }</button>`;
-    listItem.innerHTML = content;
-    searchHistoryEl.appendChild(listItem);
-  } else {
-    return;
+  if (locationHistory !== null && !locationHistory.includes(location)) {
+    let duplicateLocation = function checkForDuplicate(searches, location) {
+      return searches.some(function (loc) {
+        return location.toLowerCase() === loc.toLowerCase();
+      });
+    };
+
+    if (!duplicateLocation(locationHistory, location)) {
+      let listItem = document.createElement("li");
+      let content = `<button data-location="${location}">${
+        location[0].toUpperCase() + location.substring(1)
+      }</button>`;
+      listItem.innerHTML = content;
+      searchHistoryEl.appendChild(listItem);
+      storeLocationButton(content);
+    }
   }
 }
 
@@ -39,17 +50,43 @@ function updateContentPane(event) {
   let location = buttonClicked.getAttribute("data-location");
 }
 
-function setLocalstorage(location) {
+function storeLocationButton(newButton) {
+  let locationButtons = [];
+  let recentButtons = [];
+  locationButtons.push(newButton);
+  recentButtons = [...new Set(locationButtons)];
+  localStorage.setItem("locationButtons", JSON.stringify(recentButtons));
+}
+
+function storeSearch(location) {
+  let locationSearch = [];
+  let recentSearches = [];
   locationSearch.push(location.toLowerCase());
   recentSearches = [...new Set(locationSearch)];
   localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
 }
 
-function handleGoodFetch(data, location) {
-  createLocationButton(location);
-  setLocalstorage(location);
+function createLocationObject(data) {
+  let locationObjects = [];
+  let recentObjects = [];
+  let newLocation = {
+    locationName: data.list[0].name + ", " + data.list[0].sys.country,
+    currentTemp: data.list[0].main.temp + "°F",
+    wind: data.list[0].wind.speed + " mi/h",
+    humidity: data.list[0].main.humidity + "%",
+    lat: data.list[0].coord.lat,
+    lon: data.list[0].coord.lon,
+  };
+  locationObjects.push(JSON.stringify(newLocation));
+  recentObjects = [...new Set(locationObjects)];
+  localStorage.setItem("locationObjects", recentObjects);
+}
 
-  // update the main content area
+function handleGoodFetch(data, location) {
+  storeSearch(location);
+  createLocationButton(location);
+  createLocationObject(data);
+
   // fetch 5-day forecast
 }
 
@@ -60,12 +97,13 @@ function getLocation(event) {
   if (location === "") {
     return;
   } else {
-    searchURL = `${apiURL}data/2.5/find?q=${location}&appid=${appID}`;
+    searchURL = `${apiURL}data/2.5/find?q=${location}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${appID}`;
   }
   fetch(searchURL)
     .then(function (response) {
       if (!response.ok) {
-        console.log(response.status);
+        window.alert("No Location Found!");
+        return;
       }
       return response.json();
     })
@@ -76,13 +114,11 @@ function getLocation(event) {
       } else {
         handleGoodFetch(data, location);
       }
-    }); /*
-    .catch(function () {
-      window.alert("Something went wrong!");
-    });*/
+    });
 }
 
 function clearLocations() {
+  recentSearches = [];
   localStorage.clear();
 }
 function setEventListeners() {
